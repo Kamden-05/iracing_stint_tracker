ee17b7607600d8f8c6db409e5f2db062fbfabd08
Kamden <kamden@agada-medical.com>
2025-07-17 12:51:36 -0500

    moved format_time to main

--- stint.py
+++ stint.py
@@ -1,14 +1,7 @@
 from dataclasses import dataclass
 from typing import List
 from datetime import datetime
-
-
-def format_time(seconds: float) -> str:
-    h, m = divmod(int(seconds), 3600)
-    m, s = divmod(m, 60)
-    ms = seconds % 1
-    s_with_ms = s + ms
-    return f"{h}:{m:02}:{s_with_ms:06.3f}" if h > 0 else f"{m:02}:{s_with_ms:06.3f}"
+from main import format_time
 
 
 @dataclass
