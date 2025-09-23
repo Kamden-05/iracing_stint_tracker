2c8f367c33addb334b9b3253fd91a11e59c1503f
Kamden <kamden@agada-medical.com>
2025-09-22 20:10:10 -0500

    Fixed lap timings

--- src/session_manager.py
+++ src/session_manager.py
@@ -10,9 +10,10 @@ class SessionManager:
         self.ir = ir or irsdk.IRSDK()
         self.is_connected: bool = False
         self.stints: List[Stint] = []
-        self.stint_count: int = 0
         self.prev_pit_active: bool = False
         self.current_stint: Stint = None
+        self.last_recorded_lap = -1
+        self.end_stint = False
 
     def connect(self) -> bool:
         if not self.is_connected:
@@ -33,23 +34,44 @@ class SessionManager:
         self.ir.freeze_var_buffer_latest()
         car_id = self.ir["PlayerCarIdx"]
         pit_active = self.ir["PitstopActive"]
+        lap = self.ir["LapCompleted"]
 
         """ 
         if pit inactive and no stint, start next stint
         if pit active and last pit inactive, record the pit data
-        if pit inactive and last pit active, end the stint
+        if pit inactive and last pit active, change the flag so we know to end the stint
         """
         if not pit_active and self.current_stint is None:
             self.current_stint = self._start_stint(car_id)
+
         elif pit_active and not self.prev_pit_active:
             self.current_stint = self._record_pit(self.current_stint)
+
         elif not pit_active and self.prev_pit_active:
-            self.current_stint = self._end_stint(self.current_stint)
-            self.stints.append(self.current_stint)
-            self.current_stint = None
+            self.end_stint = True
 
-        # TODO: record lap times
+        # record laps, make sure that the in lap has only been recorded once we've pit and crossed the start/finish line
+        if self.current_stint:
+            lap_time = self.ir["LapLastLapTime"]
 
+            if (
+                lap_time > 0.0
+                and lap != self.last_recorded_lap
+                and (
+                    len(self.current_stint.laps) == 0
+                    or (lap_time != self.current_stint.laps[-1])
+                )
+            ):
+                print(f"Recording lap {lap}: {lap_time}")
+                self.current_stint.laps.append(lap_time)
+                self.last_recorded_lap = lap
+
+            if self.end_stint:
+                self.current_stint = self._end_stint(self.current_stint)
+                self.stints.append(self.current_stint)
+                print(self.current_stint.display())
+                self.current_stint = None
+                self.end_stint = False
         self.prev_pit_active = pit_active
 
     def _start_stint(self, car_id: int) -> Stint:
@@ -57,7 +79,7 @@ class SessionManager:
             driver=self.ir["DriverInfo"]["Drivers"][car_id]["UserName"],
             laps=[],
             start_time=self.ir["SessionTime"],
-            start_position=self.ir["CarIdxClassPosition"],
+            start_position=self.ir["PlayerCarClassPosition"],
             start_incidents=self.ir["PlayerCarMyIncidentCount"],
             start_fuel=self.ir["FuelLevel"],
             start_fast_repairs=self.ir["FastRepairUsed"],
@@ -67,10 +89,13 @@ class SessionManager:
 
     def _end_stint(self, stint: Stint) -> Stint:
         stint.stint_length = self.ir["SessionTime"] - stint.start_time
-        stint.end_position = self.ir["CarIdxClassPosition"]
+        stint.end_position = self.ir["PlayerCarClassPosition"]
         stint.incidents = self.ir["PlayerCarMyIncidentCount"] - stint.start_incidents
-        stint.out_lap = stint.laps[1] if stint.laps else 0.0
+        stint.out_lap = stint.laps[0] if stint.laps else 0.0
         stint.in_lap = stint.laps[-1] if stint.laps else 0.0
+        stint.avg_lap = (
+            float(sum(stint.laps)) / float(len(stint.laps)) if stint.laps else 0.0
+        )
         return stint
 
     def _record_pit(self, stint: Stint) -> Stint:
@@ -80,6 +105,7 @@ class SessionManager:
         stint.refuel_amount = max(self.ir["dpFuelAddKg"] - stint.end_fuel, 0.0)
         stint.repairs = self._check_repairs(stint)
         stint.tire_change = self._check_tires()
+
         return stint
 
     def _check_repairs(self, stint: Stint) -> bool:
--- src/stint.py
+++ src/stint.py
@@ -21,6 +21,7 @@ class Stint:
     incidents: Optional[int] = None
     in_lap: Optional[float] = None
     out_lap: Optional[float] = None
+    avg_lap: Optional[float] = None
     laps_completed: Optional[int] = None
     end_position: Optional[int] = None
 
@@ -33,3 +34,31 @@ class Stint:
     service_time: Optional[float] = None
     tire_change: Optional[bool] = False
     repairs: Optional[bool] = False
+
+    def display(self) -> None:
+        """Print all values in the stint for easy review."""
+        print(f"{'='*40}")
+        print(f"Driver: {self.driver}")
+        print(f"Start Time: {self.start_time}")
+        print(f"Start Position: {self.start_position}")
+        print(f"Start Incidents: {self.start_incidents}")
+        print(f"Start Fuel: {self.start_fuel}")
+        print(f"Start Fast Repairs: {self.start_fast_repairs}")
+        print(f"Laps: {self.laps}")
+        print(f"{'-'*40}")
+        print(f"Stint Length: {self.stint_length}")
+        print(f"End Position: {self.end_position}")
+        print(f"Incidents in Stint: {self.incidents}")
+        print(f"Out Lap: {self.out_lap}")
+        print(f"In Lap: {self.in_lap}")
+        print(f"Average Lap: {self.avg_lap}")
+        print(f"Laps Completed: {self.laps_completed}")
+        print(f"{'-'*40}")
+        print(f"End Fuel: {self.end_fuel}")
+        print(f"Refuel Amount: {self.refuel_amount}")
+        print(f"Required Repair Time: {self.required_repair_time}")
+        print(f"Optional Repair Time: {self.optional_repair_time}")
+        print(f"Service Time: {self.service_time}")
+        print(f"Tire Change: {self.tire_change}")
+        print(f"Repairs: {self.repairs}")
+        print(f"{'='*40}\n")
