1283a26233bb7161a614754f556280e8fcf6038f
Kamden <kamden@agada-medical.com>
2025-10-20 18:15:56 -0500

    refactored api_client as a class

--- src/api_client.py
+++ src/api_client.py
@@ -4,29 +4,46 @@ from src.stint import Lap
 from typing import Any
 
 
-def get_latest_stint(url: str, session_id: int):
-    r = requests.get(f"{url}/sessions/{session_id}/latest")
-    print(r.text)
+class APIClient:
+    def __init__(
+        self,
+        base_url: str,
+    ):
+        self.base_url = base_url.rstrip("/")
+        self.session = requests.Session()
+
+    def get_latest_stint(self, session_id: int):
+        r = requests.get(f"{self.base_url}/sessions/{session_id}/stints/latest")
+        print(r.text)
+
+    def post_session(self, session_info: dict):
+        payload = json.dumps(session_info)
+        r = requests.post(f"{self.base_url}/sessions/", data=payload)
+        print(r.text)
+
+    def post_stint(self, session_id: int, stint_json: dict[str, Any]):
+        r = requests.post(
+            f"{self.base_url}/sessions/{session_id}/stints/", data=stint_json
+        )
+        print(r.text)
+
+    def put_stint(self, session_id: int, stint_json: dict[str, Any]):
+        r = requests.put(f"{self.base_url}/{session_id}/stints/", data=stint_json)
+        print(r.text)
+
+    def post_lap(self, stint_id: int, lap: Lap):
+        lap = Lap(stint_id=10, time=60.5, number=1)
+        payload = json.dumps(lap.to_dict())
+        r = requests.post(f"{self.base_url}/stints/{stint_id}/laps/", data=payload)
+        print(r.text)
+
+    def close(self):
+        self.session.close()
 
 
-def post_session(url: str, session_info: dict):
-    payload = json.dumps(session_info)
-    r = requests.post(f"{url}/sessions/", data=payload)
-    print(r.text)
+url = "http://127.0.0.1:8000"
+id = 0
 
+client = APIClient(base_url=url)
 
-def post_stint(url: str, session_id: int, stint_json: dict[str, Any]):
-    r = requests.post(f"{url}/sessions/{session_id}/stints/", data=stint_json)
-    print(r.text)
-
-
-def put_stint(url: str, session_id: int, stint_json: dict[str, Any]):
-    r = requests.put(f"{url}/{session_id}/stints/", data=stint_json)
-    print(r.text)
-
-
-def post_lap(url: str, stint_id: int, lap: Lap):
-    lap = Lap(stint_id=10, time=60.5, number=1)
-    payload = json.dumps(lap.to_dict())
-    r = requests.post(f"{url}/stints/{stint_id}/laps/", data=payload)
-    print(r.text)
+client.get_latest_stint(session_id=id)
