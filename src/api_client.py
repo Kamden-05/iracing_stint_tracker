3b6626325c38415b5a64e730900ae3aec79d3613
Kamden <kamden@agada-medical.com>
2025-10-20 20:18:57 -0500

    changed methods to handle dict instead of json

--- src/api_client.py
+++ src/api_client.py
@@ -25,8 +25,7 @@ class APIClient:
     """Session Methods"""
 
     def post_session(self, session_info: dict):
-        payload = json.dumps(session_info)
-        r = self.s.post(f"{self.base_url}/sessions/", data=payload)
+        r = self.s.post(f"{self.base_url}/sessions/", json=session_info)
         print(r.text)
 
     """Stint Methods"""
@@ -35,22 +34,23 @@ class APIClient:
         r = self.s.get(f"{self.base_url}/sessions/{session_id}/stints/latest")
         print(r.text)
 
-    def post_stint(self, session_id: int, stint_json: dict[str, Any]):
+    def post_stint(self, stint_data: dict):
+        session_id = stint_data['session_id']
         r = self.s.post(
-            f"{self.base_url}/sessions/{session_id}/stints/", data=stint_json
+            f"{self.base_url}/sessions/{session_id}/stints/", json=stint_data
         )
         print(r.text)
 
-    def put_stint(self, session_id: int, stint_json: dict[str, Any]):
-        r = self.s.put(f"{self.base_url}/{session_id}/stints/", data=stint_json)
+    def put_stint(self, stint_data: dict):
+        session_id = stint_data['session_id']
+        r = self.s.put(f"{self.base_url}/{session_id}/stints/", json=stint_data)
         print(r.text)
 
     """Lap Methods"""
 
     def post_lap(self, stint_id: int, lap: Lap):
         lap = Lap(stint_id=1, time=60.5, number=2)
-        payload = json.dumps(lap.to_dict())
-        r = self.s.post(f"{self.base_url}/stints/{stint_id}/laps/", data=payload)
+        r = self.s.post(f"{self.base_url}/stints/{stint_id}/laps/", json=lap.to_dict())
         print(r.text)
 
 
