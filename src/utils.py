04b03746d0495ec4ca66bd031c8b6e94cc63c98d
Kamden-05 <kamdenw05@gmail.com>
2025-10-24 13:22:29 -0500

    removed sheet id method

--- src/utils.py
+++ src/utils.py
@@ -8,13 +8,6 @@ def format_time(seconds: float) -> str:
     s_with_ms = s + ms
     return f"{h}:{m:02}:{s_with_ms:06.3f}" if h > 0 else f"{m:02}:{s_with_ms:06.3f}"
 
-def get_sheet_id(url: str) -> str:
-    match = re.search(r"d/([^/]+)/edit", url)
-    print(match.group())
-    if match:
-        return match.group(1)
-    return ""
-
 def get_task_dict(task_type: str, data: Any) -> dict:
     return {
         'type': task_type,
