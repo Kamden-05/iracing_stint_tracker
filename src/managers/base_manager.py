9ccec8305258ea565e263b0c30decc8446fcc266
Kamden-05 <kamdenw05@gmail.com>
2025-11-19 16:56:42 -0600

    added required fields to base manager

--- src/managers/base_manager.py
+++ src/managers/base_manager.py
@@ -1,6 +1,8 @@
 from src.fsm.driver_fsm import States
 
 class BaseManager:
+    required_fields = set[str] = set()
+
     def handle_event(self, event: str, telem: dict[str, any], ctx: dict[str, any]):
         pass
 
