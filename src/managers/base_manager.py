e8f49bc44d63876c2b5d7eaef12054752e5397e3
Kamden-05 <kamdenw05@gmail.com>
2025-11-20 00:09:55 -0600

    added context to base manager

--- src/managers/base_manager.py
+++ src/managers/base_manager.py
@@ -1,11 +1,13 @@
 from queue import Queue
 from typing import Any
 from src.fsm.driver_fsm import States
+from src.context.session_context import SessionContext
 
 class BaseManager:
     required_fields: set[str] = set()
 
-    def __init__(self, queue: Queue):
+    def __init__(self, context: SessionContext, queue: Queue):
+        self.context = context
         self.queue = queue
 
     def handle_event(self, event: str, telem: dict[str, Any], ctx: dict[str, Any]):
