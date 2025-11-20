c5bd1b0ba27d2fe146e6b2f789b60437048c7fd9
Kamden-05 <kamdenw05@gmail.com>
2025-11-20 00:09:42 -0600

    removed get task dict from utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,14 +1,6 @@
-from typing import Any
-
 def format_time(seconds: float) -> str:
     h, m = divmod(int(seconds), 3600)
     m, s = divmod(m, 60)
     ms = seconds % 1
     s_with_ms = s + ms
     return f"{h}:{m:02}:{s_with_ms:06.3f}" if h > 0 else f"{m:02}:{s_with_ms:06.3f}"
-
-def get_task_dict(task_type: str, data: Any) -> dict:
-    return {
-        'type': task_type,
-        'data': data
-    }
