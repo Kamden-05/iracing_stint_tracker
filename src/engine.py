a25d63bbf89c546360eae14e242b585d34b1708e
Kamden-05 <kamdenw05@gmail.com>
2025-11-20 20:14:11 -0600

    created AppEngine to run loop and managers

---
+++ src/engine.py
@@ -0,0 +1,51 @@
+from queue import Queue
+import threading
+from src.fsm.driver_fsm import DriverFSM
+from src.telemetry.iracing_client import IRacingClient
+from src.telemetry.telemetry_loop import TelemetryLoop
+from src.api.api_client import APIClient
+from src.api.api_worker import APIWorker
+from src.context.race_context import RaceContext
+
+
+class AppEngine:
+    def __init__(
+        self,
+        user_name: str,
+        api_base_url: str,
+    ):
+        self.context = RaceContext(user_name=user_name)
+        self.queue = Queue()
+        self.stop_event = threading.Event()
+
+        self.api_client = APIClient(api_base_url)
+        self.api_worker = APIWorker(self.context, self.api_client, self.queue, self.stop_event)
+
+        self.fsm = DriverFSM()
+        self.managers = []
+        self.fsm.attach_managers(self.managers)
+
+        self.telemetry_loop = TelemetryLoop(
+            ir_client=IRacingClient(),
+            fsm=self.fsm,
+            user_name=user_name,
+        )
+
+        self.api_thread = threading.Thread(
+            target=self.api_worker.run, daemon=True
+        )
+        self.telemetry_thread = threading.Thread(
+            target=self.telemetry_loop.run, daemon=True
+        )
+
+    def start(self):
+        print ("Starting API Worker")
+        self.api_thread.start()
+
+        print("Starting telemetry loop")
+        self.telemetry_thread.start()
+
+    def stop(self):
+        print("Stopping engine")
+        self.stop_event.set()
+        self.api_thread.join(timeout=2)
--- src/main.py
+++ src/main.py
@@ -1,60 +1,23 @@
-import logging
+from time import time
 import os
-import threading
-import time
-from queue import Empty, Queue
 from dotenv import load_dotenv
-from src.api.api_client import APIClient
-from src.session_manager import SessionManager, SessionStatus
-from src.utils.utils import get_task_dict
-from src.gui.app_gui import StintTrackerGUI
+from src.engine import AppEngine
 
-logger = logging.getLogger(__name__)
-
-def manage_race(manager: SessionManager, name: str, q: Queue, stop_event):
-    finished = False
-
-    try:
-        while not stop_event.is_set() and not finished:
-            manager.check_iracing()
-
-            if not manager.is_connected:
-                time.sleep(1)
-                continue
-
-            if manager.session_id is None or manager.session_id != manager.get_ir_session_id():
-                manager.init_session()
-                session_task = get_task_dict(
-                    task_type="Session", data=manager.get_session_info()
-                )
-                q.put(session_task)
-
-            session_type = manager.get_session_type()
-
-            if session_type == "Race":
-                completed_stint = manager.process_race()
-
-                if completed_stint and completed_stint.driver_name == name:
-                    stint_task = get_task_dict(
-                        task_type="Stint",
-                        data={
-                            "stint": completed_stint,
-                            "session_id": manager.session_id,
-                        },
-                    )
-                    q.put(stint_task)
-
-            if manager.status == SessionStatus.FINISHED:
-                finished = True
-                manager.disconnect()
-
-            time.sleep(1 / 60)
-    finally:
-        manager.disconnect()
-        q.put(None)
 
 def main():
-    pass
+    load_dotenv()
+    api_url = os.getenv("TEST_URL")
+    user_name = "Kam Wilson"
+
+    engine = AppEngine(user_name=user_name, api_base_url=api_url)
+
+    engine.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        engine.stop()
 
 
 if __name__ == "__main__":
