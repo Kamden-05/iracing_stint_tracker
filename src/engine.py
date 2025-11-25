cc948e9a023ecfb05651dd5daec26ca30fba62aa
Kamden-05 <kamdenw05@gmail.com>
2025-11-25 00:56:36 -0600

    added session change event listener

--- src/engine.py
+++ src/engine.py
@@ -21,6 +21,7 @@ class AppEngine:
         self.context = RaceContext(user_name=user_name)
         self.queue = Queue()
         self.stop_event = threading.Event()
+        self.session_reset_event = threading.Event()
         self.user_name = user_name
         self.api_url = api_base_url
 
@@ -28,13 +29,18 @@ class AppEngine:
         self._setup_fsm_managers()
         self._setup_telemetry()
 
+        self.session_reset_thread = threading.Thread(
+            target=self._session_reset_watcher, daemon=True
+        )
+
     def _setup_api(self):
         self.api_client = APIClient(self.api_url)
-        self.api_worker = APIWorker(self.context, self.api_client, self.queue, self.stop_event)
-
-        self.api_thread = threading.Thread(
-            target=self.api_worker.run, daemon=True
+        self.api_worker = APIWorker(
+            self.context, self.api_client, self.queue, self.stop_event
         )
+
+        self.api_thread = threading.Thread(target=self.api_worker.run, daemon=True)
+
     def _setup_fsm_managers(self):
         self.fsm = DriverFSM()
         self.managers = [
@@ -48,31 +54,44 @@ class AppEngine:
             ir_client=IRacingClient(),
             fsm=self.fsm,
             user_name=self.user_name,
+            session_reset_event=self.session_reset_event
         )
 
         self.telemetry_thread = threading.Thread(
             target=self.telemetry_loop.run, daemon=True
         )
 
+    def _session_reset_watcher(self):
+        while not self.stop_event.is_set():
+            if self.session_reset_event.wait(timeout=1):
+                self.session_reset_event.clear()
+                self.reset()
+
     def start(self):
-        print ("Starting API Worker")
+        print("Starting API Worker")
         self.api_thread.start()
 
         print("Starting telemetry loop")
         self.telemetry_thread.start()
 
+        print("Starting session reset watcher")
+        self.session_reset_thread.start()
+
     def stop(self):
         print("Stopping engine")
         self.stop_event.set()
-
-        self.api_thread.join(timeout=2)
+        self.api_thread.join()
+        self.telemetry_loop.stop()
+        self.telemetry_thread.join()
 
     def reset(self):
+        print("Resetting engine...")
         self.telemetry_loop.stop()
-        self.telemetry_thread.join(timeout=2)
+        self.telemetry_thread.join()
 
         self.context.reset()
 
         self._setup_fsm_managers()
         self._setup_telemetry()
+
         self.telemetry_thread.start()
