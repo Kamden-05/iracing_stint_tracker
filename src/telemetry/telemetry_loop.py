cded1179fcf7eb8015af1d5ea0f021ae7284b4e5
Kamden-05 <kamdenw05@gmail.com>
2026-03-29 18:38:22 -0500

    fixed check race start logic

--- src/telemetry/telemetry_loop.py
+++ src/telemetry/telemetry_loop.py
@@ -24,6 +24,7 @@ class TelemetryLoop:
         self.fsm: "DriverFSM" = fsm
         self.interval: float = 1.0 / hz
 
+        self.prev_user_in_car: bool = False
         self.prev_session_id: Optional[int] = None
         self.prev_on_track: bool = False
         self.prev_on_pit_road: bool = False
@@ -45,7 +46,8 @@ class TelemetryLoop:
 
     def _check_race_start(self) -> bool:
         return (
-            self.ir.get("SessionState") == SessionState.racing
+            self.ir.get("SessionNum") == 2  # 0 for practice, 1 for quali, 2 for race
+            and self.ir.get("SessionState") == SessionState.racing
             and self.ir.get("PlayerCarClassPosition") > 0
         )
 
@@ -115,36 +117,36 @@ class TelemetryLoop:
 
             # FSM transitions
 
-            # TODO: dont make non session related transitions unless the session has actually started
-            # TODO: make sure driver is in the car before we call certain transitions
+            user_is_driving = on_track or (tow_time > 0.0 and self.prev_user_in_car)
 
             # session start
             if self._check_race_start() and not self.session_started:
                 self.session_started = True
                 self.fsm.session_start()
 
-            # enter pit road
-            if not self.prev_on_pit_road and (on_pit_road or tow_time > 0.0):
-                self.fsm.enter_pit_road()
+            if self.session_started:
+                if user_is_driving:
+                    # enter pit road
+                    if not self.prev_on_pit_road and (on_pit_road or tow_time > 0.0):
+                        self.fsm.enter_pit_road()
 
-            # exit pit road
-            if self.prev_on_pit_road and not on_pit_road:
-                self.fsm.exit_pit_road()
+                    # exit pit road
+                    if self.prev_on_pit_road and not on_pit_road:
+                        self.fsm.exit_pit_road()
 
-            # enter pit box
-            if not self.prev_in_pit_box and pit_active:
-                self.fsm.enter_pit_box()
+                    # enter pit box
+                    if not self.prev_in_pit_box and pit_active:
+                        self.fsm.enter_pit_box()
 
-            # exit pit box
-            if self.prev_in_pit_box and not pit_active:
-                self.fsm.exit_pit_box()
+                    # exit pit box
+                    if self.prev_in_pit_box and not pit_active:
+                        self.fsm.exit_pit_box()
 
-            # driver swaps
-            if self.session_started and tow_time == 0.0:
-                if not on_track and self.prev_on_track:
+                # driver swaps
+                if self.prev_user_in_car and not user_is_driving:
                     self.fsm.driver_swap_out()
 
-                if on_track and not self.prev_on_track:
+                if not self.prev_user_in_car and user_is_driving:
                     self.fsm.driver_swap_in()
 
             # session finish
@@ -157,6 +159,7 @@ class TelemetryLoop:
                 m.on_tick(tick_data)
 
             # update prev values
+            self.prev_user_in_car = user_is_driving
             self.prev_on_track = on_track
             self.prev_on_pit_road = on_pit_road
             self.prev_in_pit_box = pit_active
